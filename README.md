21a0dec6e4eb5856cbb7adbaeeb1ba086aa66b21
Tatianacol123 <integralcoordinator.col@gmail.com>
2025-11-19 22:46:58 -0500

    Actualización final de proyecto

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# car_sales_app
+# Proyecto Sprint 7 – Aplicación Streamlit al análisis de datos sobre Vehículos
+Por medio de la presente aplicación web, podmeos visualizar datos de vehículos usados publicados en EE.UU, incluyendo gráficos interactivos que fueron diseñados con Streamlit y Plotly.
+
+## Funcionalidad
+- Visualizar un histograma del odómetro.
+- Visualizar un histograma del año del vehículo.
+- Analizar la relación entre el precio y el kilometraje mediante los gráficos de dispersión.
+- Interactuar con botones y elementos de control (checkbox).
+
+## Archivos del proyecto
+vehicles_us.csv — Dataset del proyecto
+notebooks/EDA.ipynb — Análisis exploratorio inicial
+App.py — Código en la aplicación Streamlit
+requirements.txt — Librerías necesarias
+
+
+## Intrucción para la ejecución
+1. Creación del entorno virtual
+   python -m venv vehicles_env
+
+2. Activación:
+   Windows PowerShell:
+   .\vehicles_env\Scripts\Activate.ps1
+
+3. Instalación de dependencias:
+   pip install -r requirements.txt
+
+4. Ejecución:
+   streamlit run app.py
--- app.py
+++ app.py
@@ -1,26 +1,21 @@
 import pandas as pd
 import plotly.express as px
 import streamlit as st
-     
-car_data = pd.read_csv('vehicles_us.csv') # leer los datos
-hist_button = st.button('Construir histograma') # crear un botón
-     
-if hist_button: # al hacer clic en el botón
-# escribir un mensaje
+
+car_data = pd.read_csv('vehicles_us.csv')  # leer los datos
+
+# Botón
+hist_button = st.button('Construir histograma')
+
+if hist_button:
     st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
-         
-# crear un histograma
     fig = px.histogram(car_data, x="odometer")
-     
-# mostrar un gráfico Plotly interactivo
-    st.plotly_chart(fig, use_container_width=True)
+    st.plotly_chart(fig, use_container_width=True, key="hist_btn")
 
-
-# crear una casilla de verificación
+# Checkbox
 build_histogram = st.checkbox('Construir un histograma')
 
-if build_histogram: # si la casilla de verificación está seleccionada
+if build_histogram:
     st.write('Construir un histograma para la columna odómetro')
-    # crear un histograma
     fig_2 = px.histogram(car_data, x="odometer")
-    st.plotly_chart(fig_2, use_container_width=True)
+    st.plotly_chart(fig_2, use_container_width=True, key="hist_chk")
